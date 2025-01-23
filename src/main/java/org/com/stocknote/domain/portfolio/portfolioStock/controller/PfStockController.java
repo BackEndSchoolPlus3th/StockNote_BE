f5bff5d105849382a567bbaf6f6cc09ff8a15f29
kknaks <benesia93@naver.com>
2025-01-23 22:09:31 +0900

    Feature 주식종목추가 SMS-54

---
+++ src/main/java/org/com/stocknote/domain/portfolio/portfolioStock/controller/PfStockController.java
@@ -0,0 +1,46 @@
+package org.com.stocknote.domain.portfolio.portfolioStock.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.com.stocknote.domain.portfolio.portfolioStock.dto.PfStockRequest;
+import org.com.stocknote.domain.portfolio.portfolioStock.dto.StockTempResponse;
+import org.com.stocknote.domain.portfolio.portfolioStock.service.PfStockService;
+import org.com.stocknote.domain.stock.entity.Stock;
+import org.com.stocknote.global.dto.GlobalResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/portfolios/{portfolio_no}/stocks")
+public class PfStockController {
+  private final PfStockService pfStockService;
+
+  @PostMapping
+  public String postPortfolioCash() {
+    return "Portfolio post";
+  }
+
+  @PostMapping("/AddStock")
+  public String addPortfolioStock(
+      @PathVariable("portfolio_no") Long portfolioNo,
+      @RequestBody PfStockRequest pfStockRequest) {
+
+    pfStockService.savePfStock(portfolioNo, pfStockRequest);
+    return "Portfolio post";
+  }
+
+  @PatchMapping
+  public String patchPortfolioStock() {
+    return "Portfolio modify";
+  }
+
+  @DeleteMapping
+  public String deletePortfolioStock() {
+    return "Portfolio del";
+  }
+
+
+}
